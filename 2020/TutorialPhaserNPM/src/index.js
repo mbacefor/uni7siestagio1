b9aeceba053fa6d68767fae24a891bef5e9a3997
mbacefor <mbacefor@gmail.com>
2020-03-15 22:34:55 -0300

    Implementação

--- 2020/TutorialPhaserNPM/src/index.js
+++ 2020/TutorialPhaserNPM/src/index.js
@@ -7,7 +7,6 @@ import bombImg from "./assets/bomb.png";
 import dudeImg from "./assets/dude.png";
 import pessoasImg from "./assets/pessoas.png"
 
-
 var config = {
   type: Phaser.AUTO,
   width: 800,
@@ -16,7 +15,7 @@ var config = {
     default: 'arcade',
     arcade: {
       gravity: { y: 300 },
-      debug: false
+      debug: true
     }
   },
   scene: {
@@ -26,8 +25,8 @@ var config = {
   }
 };
 
-var player;
-var playerMarcelo;
+
+var players = new Array();
 var stars;
 var bombs;
 var platforms;
@@ -46,7 +45,6 @@ function preload() {
   this.load.image('bomb', bombImg);
   this.load.spritesheet('dude', dudeImg, { frameWidth: 32, frameHeight: 48 });
   this.load.spritesheet('pessoas', pessoasImg, { frameWidth: 48, frameHeight: 48 });
-
 }
 
 function create() {
@@ -66,35 +64,33 @@ function create() {
   platforms.create(750, 220, 'ground');
 
   // The player and its settings
-  player = this.physics.add.sprite(100, 450, 'dude');
-  playerMarcelo = this.physics.add.sprite(200, 450, 'pessoas');
-
+  
+  players.push(this.physics.add.sprite(100, 450, 'dude'));
+  players.push(this.physics.add.sprite(200, 450, 'pessoas'));
 
   //  Player physics properties. Give the little guy a slight bounce.
-  player.setBounce(0.2);
-  player.setCollideWorldBounds(true);
-
-   //  Player physics properties. Give the little guy a slight bounce.
-   playerMarcelo.setBounce(0.2);
-   playerMarcelo.setCollideWorldBounds(true);
-  
+  players.forEach(function (item, indice, array) {
+    console.log(item, indice);
+    item.setBounce(0.2);
+    item.setCollideWorldBounds(true);
+  });
 
   //  Our player animations, turning, walking left and walking right.
   this.anims.create({
-    key: 'left',
+    key: 'left0',
     frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
     frameRate: 10,
     repeat: -1
   });
 
   this.anims.create({
-    key: 'turn',
+    key: 'turn0',
     frames: [{ key: 'dude', frame: 4 }],
     frameRate: 20
   });
 
   this.anims.create({
-    key: 'right',
+    key: 'right0',
     frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
     frameRate: 10,
     repeat: -1
@@ -102,25 +98,24 @@ function create() {
 
   //  Our player animations, turning, walking left and walking right.
   this.anims.create({
-    key: 'leftMarcelo',
+    key: 'left1',
     frames: this.anims.generateFrameNumbers('pessoas', { start: 15, end: 17 }),
     frameRate: 10,
     repeat: -1
   });
 
   this.anims.create({
-    key: 'turnMarcelo',
+    key: 'turn1',
     frames: [{ key: 'pessoas', frame: 4 }],
     frameRate: 20
   });
 
   this.anims.create({
-    key: 'rightMarcelo',
+    key: 'right1',
     frames: this.anims.generateFrameNumbers('pessoas', { start: 27, end: 29 }),
     frameRate: 10,
     repeat: -1
   });
-
 
   //  Input Events
   cursors = this.input.keyboard.createCursorKeys();
@@ -140,25 +135,25 @@ function create() {
   });
 
   bombs = this.physics.add.group();
-  
 
   //  The score
   scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
   //  Collide the player and the stars with the platforms
-  this.physics.add.collider(player, platforms);
-  this.physics.add.collider(playerMarcelo, player);
-  this.physics.add.collider(playerMarcelo, platforms);
-  this.physics.add.collider(stars, platforms);
+    this.physics.add.collider(stars, platforms);
   this.physics.add.collider(bombs, platforms);
   this.physics.add.collider(bombs, bombs);
 
-  //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-  this.physics.add.overlap(player, stars, collectStar, null, this);
-  this.physics.add.collider(player, bombs, hitBomb, null, this);
-  this.physics.add.overlap(playerMarcelo, stars, collectStar, null, this);
-  this.physics.add.collider(playerMarcelo, bombs, hitBomb, null, this);
-
+  for (var i = 0; i < players.length; i++) {
+    var item = players[i];
+    console.log(item, i,this);
+    this.physics.add.collider(item, platforms);
+    this.physics.add.overlap(item, stars, collectStar, null, this);
+    this.physics.add.collider(item, bombs, hitBomb, null, this);
+    this.physics.add.overlap(item, stars, collectStar, null, this);
+    this.physics.add.collider(item, bombs, hitBomb, null, this);
+  }
+    
 }
 
 function update() {
@@ -167,32 +162,44 @@ function update() {
   }
 
   if (cursors.left.isDown) {
-    player.setVelocityX(-160);
-    player.anims.play('left', true);
 
-    playerMarcelo.setVelocityX(-160);
-    playerMarcelo.anims.play('leftMarcelo', true);
+    players.forEach(function (item, indice, array) {
+      console.log(item, indice);
+      item.setVelocityX(-160);
+      item.anims.play('left' + indice, true);
+    });
 
   }
   else if (cursors.right.isDown) {
-    player.setVelocityX(160);
-    player.anims.play('right', true);
-    playerMarcelo.setVelocityX(160);
-    playerMarcelo.anims.play('rightMarcelo', true);
+
+    players.forEach(function (item, indice, array) {
+      console.log(item, indice);
+      item.setVelocityX(160);
+      item.anims.play('right' + indice, true);
+    });
+
   }
-  
 
   else {
-    player.setVelocityX(0);
-    player.anims.play('turn');
-    playerMarcelo.setVelocityX(0);
-    playerMarcelo.anims.play('turnMarcelo');
+    players.forEach(function (item, indice, array) {
+      console.log(item, indice);
+      item.setVelocityX(0);
+      item.anims.play('turn' + indice, true);
+    });
+
   }
 
 
-  if (cursors.up.isDown && player.body.touching.down) {
-    player.setVelocityY(-330);
-    playerMarcelo.setVelocityY(-330);
+  if (cursors.up.isDown) {
+
+    players.forEach(function (item, indice, array) {
+      if (item.body.touching.down){
+        console.log(item, indice);
+        item.setVelocityY(-330);
+      }
+      
+    });
+
   }
 }
 
@@ -206,7 +213,6 @@ function collectStar(player, star) {
   if (stars.countActive(true) === 0) {
     //  A new batch of stars to collect
     stars.children.iterate(function (child) {
-
       child.enableBody(true, child.x, 0, true, true);
 
     });
@@ -227,41 +233,9 @@ function hitBomb(player1, bomb) {
 
   player1.setTint(0xff0000);
 
-  if (player1==player)
-    player1.anims.play('turn');
-  else player1.anims.play('turnMarcelo');
-
+  players.forEach(function (item, indice, array) {
+    if (player1 == item)
+    player1.anims.play('turn'+indice);  
+  });
   gameOver = true;
 }
-
-
-/* const config = {
-  type: Phaser.AUTO,
-  parent: "phaser-example",
-  width: 800,
-  height: 600,
-  scene: {
-    preload: preload,
-    create: create
-  }
-};
-
-const game = new Phaser.Game(config);
-
-function preload() {
-  this.load.image("logo", logoImg);
-}
-
-function create() {
-  const logo = this.add.image(400, 150, "logo");
-
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
-}
- */
